84ee7d20f6136ef2ae6c64ee198948930ca58301
May Sy <maysy@Mays-MacBook-Pro-2.local>
2024-11-19 10:55:56 -0800

    ignored files

--- app.py
+++ app.py
@@ -8,4 +8,16 @@ def index():
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
+
+    filtered_fruits_o = []
+    for fruit in fruits:
+        if fruit.get("name", "").startswith("o"):
+            filtered_fruits_o.append(fruit["name"])
+
+    more_than_three = []
+    for fruit in fruits:
+        if fruit.get("quantity", 0) > 3:
+            more_than_three.append(fruit["name"])
+
     return render_template("index.html", fruits=fruits)
+    # return render_template("index.html", filtered_fruits_o=filtered_fruits_o)
